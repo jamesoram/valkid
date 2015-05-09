e7366edc96f5c682af2952fababa1f1a2400266e
James Oram <herespam1@hotmail.com>
2015-05-09 21:42:38 +0100

    add user entity

---
+++ src/main/java/io/tromba/valkid/entity/User.java
@@ -0,0 +1,67 @@
+package io.tromba.valkid.entity;
+
+import org.bson.types.ObjectId;
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
+import org.mongodb.morphia.annotations.Entity;
+import org.mongodb.morphia.annotations.Id;
+import org.mongodb.morphia.annotations.Indexed;
+
+/**
+ * User entity.
+ */
+
+@Entity("users")
+public class User {
+
+    @Id
+    private ObjectId id;
+
+    @NotEmpty
+    @Length(max = 20)
+    private String firstName;
+
+    @Length(max = 20)
+    private String lastName;
+
+    @NotEmpty
+    @Indexed
+    @Email
+    private String email;
+
+    @NotEmpty
+    private String password;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
