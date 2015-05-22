3e47283a547d6c40727a395f90c9a76a0f09b4e4
James Oram <herespam1@hotmail.com>
2015-05-22 23:58:10 +0100

    add test for successful users find

--- src/test/java/io/tromba/valkid/db/TestUserDao.java
+++ src/test/java/io/tromba/valkid/db/TestUserDao.java
@@ -4,10 +4,16 @@ import io.tromba.valkid.resources.UserResource;
 import org.mockito.Mockito;
 import org.mongodb.morphia.Datastore;
 import org.mongodb.morphia.Key;
+import org.mongodb.morphia.query.Query;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.not;
 import static org.mockito.Mockito.when;
 
 /**
@@ -30,6 +36,22 @@ public class TestUserDao {
         userResource.createUser(firstName, lastName, email, password, "empty");
         // check that the data has been saved to the database.
         Mockito.verify(datastore, Mockito.times(1));
+    }
+
+    @Test(groups = "UserDao")
+    public void testUsersFound() {
+        Datastore datastore = Mockito.mock(Datastore.class);
+        List<User> users = new ArrayList<User>();
+        users.add(new User());
+        Query<User> userQuery = Mockito.mock(Query.class);
+        when(datastore.find(User.class)).thenReturn(userQuery);
+        when(datastore.find
+                (User.class)
+                .asList())
+                .thenReturn(users);
+
+        UserDao userDao = new UserDao(datastore);
+        assertThat(userDao.findAll(), not(empty()));
     }
 
     // test for find when db empty and nonempty
