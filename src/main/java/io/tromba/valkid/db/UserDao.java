0b2e8313d54a455926b5ef6413a2b94cbca51daf
James Oram <herespam1@hotmail.com>
2015-06-03 20:34:38 +0100

    add logging to user dao

--- src/main/java/io/tromba/valkid/db/UserDao.java
+++ src/main/java/io/tromba/valkid/db/UserDao.java
@@ -5,6 +5,8 @@ import io.tromba.valkid.helpers.PasswordManager;
 import org.mongodb.morphia.Datastore;
 
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Data access object for users.
@@ -12,12 +14,14 @@ import java.util.List;
 public class UserDao {
 
     private Datastore dataStore;
+    private static Logger LOGGER = Logger.getLogger(UserDao.class.getName());
 
     public UserDao(Datastore dataStore) {
         this.dataStore = dataStore;
     }
 
     public User create(String firstName, String lastName, String email, String password) {
+        LOGGER.log(Level.INFO, "dao creating user " + email);
         User user = new User();
         user.setFirstName(firstName);
         user.setLastName(lastName);
@@ -29,6 +33,7 @@ public class UserDao {
     }
 
     private void save(User user) {
+        LOGGER.log(Level.INFO, "saving user " + user.getEmail());
         String currentTime = String.valueOf(System.currentTimeMillis());
         if (null == user.getJoinDate()) {
             user.setJoinDate(currentTime);
@@ -38,6 +43,7 @@ public class UserDao {
     }
 
     private void setPassword(User user, String password) {
+        LOGGER.log(Level.INFO, "setting password for user " + user.getEmail());
         PasswordManager passwordManager = new PasswordManager();
         final String salt = passwordManager.getSalt();
         user.setSalt(salt);
@@ -46,18 +52,22 @@ public class UserDao {
 
 
     public List<User> findAll() {
+        LOGGER.log(Level.INFO, "finding all users");
         return dataStore.find(User.class).asList();
     }
 
     public User findByEmail(String email) throws NoSuchUserException {
+        LOGGER.log(Level.INFO, "finding user " + email);
         User user = dataStore.find(User.class).field("email").equal(email).get();
         if (null == user) {
+            LOGGER.log(Level.WARNING, "user not found: " + email);
             throw new NoSuchUserException();
         }
         return user;
     }
 
     public void deleteByEmail(String email) throws NoSuchUserException {
+        LOGGER.log(Level.INFO, "deleting user " + email);
         User result = dataStore
                 .findAndDelete(
                         dataStore
@@ -65,6 +75,7 @@ public class UserDao {
                                 .field("email")
                                 .equal(email));
         if (null == result) {
+            LOGGER.log(Level.WARNING, "user not found: " + email);
             throw new NoSuchUserException();
         }
     }
