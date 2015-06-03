7edc82081d1852e8f1ff66dc4c239b7b87c7c8dd
James Oram <herespam1@hotmail.com>
2015-06-03 20:30:51 +0100

    add logging to user resource

--- src/main/java/io/tromba/valkid/resources/UserResource.java
+++ src/main/java/io/tromba/valkid/resources/UserResource.java
@@ -8,6 +8,8 @@ import io.tromba.valkid.exceptions.NoSuchUserException;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * User resource class.
@@ -18,6 +20,7 @@ import java.util.List;
 public class UserResource {
 
     private final UserDao userDao;
+    private static final Logger LOGGER = Logger.getLogger(UserResource.class.getName());
 
     public UserResource(UserDao userDao) {
         this.userDao = userDao;
@@ -28,6 +31,7 @@ public class UserResource {
     public User createUser(@FormParam("first_name") String firstName, @FormParam("last_name") String lastName,
                               @FormParam("email") String email, @FormParam("password") String password,
                               @FormParam("app_token") String appToken) {
+        LOGGER.log(Level.INFO, "user creation requested, email: " + email);
         return userDao.create(firstName, lastName, email, password);
     }
 
@@ -35,12 +39,14 @@ public class UserResource {
     @Path("/all")
     @Timed
     public List<User> getUsers() {
+        LOGGER.log(Level.INFO, "find all users requested");
         return userDao.findAll();
     }
 
     @GET
     @Timed
     public User getUser(@PathParam("email") String email) {
+        LOGGER.log(Level.INFO, "get user requested, email: " + email);
         try {
             return userDao.findByEmail(email);
         } catch (NoSuchUserException e) {
@@ -51,6 +57,7 @@ public class UserResource {
     @DELETE
     @Timed
     public void deleteUser(@FormParam("email") String email) {
+        LOGGER.log(Level.INFO, "delete user requested, email: " + email);
         try {
             userDao.deleteByEmail(email);
         } catch (NoSuchUserException nse) {
